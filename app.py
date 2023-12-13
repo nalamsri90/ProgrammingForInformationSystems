cfde1e56b4edc8993b19274acc5627b54a832213
20007384 <150052047+20007384@users.noreply.github.com>
2023-12-13 17:38:49 +0000

    added missing datetime module

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask import request
+from datetime import datetime
 
 app = Flask(__name__)
 
