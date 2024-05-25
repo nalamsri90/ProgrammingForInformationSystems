a7563864aca5609b5bd39eed7bcd2bc7beb73d98
nalamsri90 <150025618+nalamsri90@users.noreply.github.com>
2024-05-25 21:03:16 +0100

    imported required libraries

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask import request
 from datetime import datetime
+from flask_cors import core
 
 app = Flask(__name__)
 
